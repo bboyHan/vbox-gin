7d58bbe084cedbc6cde11488c724edb20f9c34ef
lianxiaobao <36557317+lianxiaobao@users.noreply.github.com>
2023-08-22 15:48:52 +0800

    add:  channelshop js

---
+++ web/src/api/channelshop.js
@@ -0,0 +1,114 @@
+import service from '@/utils/request'
+
+// @Tags ChannelShop
+// @Summary 创建ChannelShop
+// @Security ApiKeyAuth
+// @accept application/json
+// @Produce application/json
+// @Param data body model.ChannelShop true "创建ChannelShop"
+// @Success 200 {string} string "{"success":true,"data":{},"msg":"获取成功"}"
+// @Router /chShop/createChannelShop [post]
+export const createChannelShop = (data) => {
+  return service({
+    url: '/chShop/createChannelShop',
+    method: 'post',
+    data
+  })
+}
+
+// @Tags ChannelShop
+// @Summary 删除ChannelShop
+// @Security ApiKeyAuth
+// @accept application/json
+// @Produce application/json
+// @Param data body model.ChannelShop true "删除ChannelShop"
+// @Success 200 {string} string "{"success":true,"data":{},"msg":"删除成功"}"
+// @Router /chShop/deleteChannelShop [delete]
+export const deleteChannelShop = (data) => {
+  return service({
+    url: '/chShop/deleteChannelShop',
+    method: 'delete',
+    data
+  })
+}
+
+// @Tags ChannelShop
+// @Summary 删除ChannelShop
+// @Security ApiKeyAuth
+// @accept application/json
+// @Produce application/json
+// @Param data body request.IdsReq true "批量删除ChannelShop"
+// @Success 200 {string} string "{"success":true,"data":{},"msg":"删除成功"}"
+// @Router /chShop/deleteChannelShop [delete]
+export const deleteChannelShopByIds = (data) => {
+  return service({
+    url: '/chShop/deleteChannelShopByIds',
+    method: 'delete',
+    data
+  })
+}
+
+// @Tags ChannelShop
+// @Summary 更新ChannelShop
+// @Security ApiKeyAuth
+// @accept application/json
+// @Produce application/json
+// @Param data body model.ChannelShop true "更新ChannelShop"
+// @Success 200 {string} string "{"success":true,"data":{},"msg":"更新成功"}"
+// @Router /chShop/updateChannelShop [put]
+export const updateChannelShop = (data) => {
+  return service({
+    url: '/chShop/updateChannelShop',
+    method: 'put',
+    data
+  })
+}
+
+// @Tags ChannelShop
+// @Summary 用id查询ChannelShop
+// @Security ApiKeyAuth
+// @accept application/json
+// @Produce application/json
+// @Param data query model.ChannelShop true "用id查询ChannelShop"
+// @Success 200 {string} string "{"success":true,"data":{},"msg":"查询成功"}"
+// @Router /chShop/findChannelShop [get]
+export const findChannelShop = (params) => {
+  return service({
+    url: '/chShop/findChannelShop',
+    method: 'get',
+    params
+  })
+}
+
+// @Tags ChannelShop
+// @Summary 分页获取ChannelShop列表
+// @Security ApiKeyAuth
+// @accept application/json
+// @Produce application/json
+// @Param data query request.PageInfo true "分页获取ChannelShop列表"
+// @Success 200 {string} string "{"success":true,"data":{},"msg":"获取成功"}"
+// @Router /chShop/getChannelShopList [get]
+export const getChannelShopList = (params) => {
+  return service({
+    url: '/chShop/getChannelShopList',
+    method: 'get',
+    params
+  })
+}
+
+
+// @Tags ChannelShop
+// @Summary 分页获取ChannelShop列表
+// @Security ApiKeyAuth
+// @accept application/json
+// @Produce application/json
+// @Param data query request.PageInfo true "分页获取ChannelShop列表"
+// @Success 200 {string} string "{"success":true,"data":{},"msg":"获取成功"}"
+// @Router /chShop/getChannelShopListByChanelRemark [get]
+export const getChannelShopListByChanelRemark = (params) => {
+  return service({
+    url: '/chShop/getChannelShopListByChanelRemark',
+    method: 'get',
+    params
+  })
+}
