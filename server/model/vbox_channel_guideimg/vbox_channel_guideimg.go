2200a8b8cd5140db961abc9af457b1d5fc53b8b1
lianxiaobao <36557317+lianxiaobao@users.noreply.github.com>
2023-08-31 00:47:59 +0800

    ix server img upload filed type

--- server/model/vbox_channel_guideimg/vbox_channel_guideimg.go
+++ server/model/vbox_channel_guideimg/vbox_channel_guideimg.go
@@ -8,8 +8,8 @@ import (
 // Channel_guideimg 结构体
 type ChannelGuideImg struct {
 	global.GVA_MODEL
-	ChannelId  string `json:"c_channel_id" form:"c_channel_id" gorm:"column:c_channel_id;comment:用户id;size:50;"`
-	ImgBaseStr string `json:"img_base_str" form:"img_base_str" gorm:"column:img_base_str;comment:通道ID;size:500;"`
+	ChannelId  string `json:"c_channel_id" form:"c_channel_id" gorm:"column:c_channel_id;comment:通道ID;size:50;"`
+	ImgBaseStr string `json:"img_base_str" form:"img_base_str" gorm:"column:img_base_str;comment:图片base64编码;type:text;"`
 	ImgNum     *int   `json:"img_num" form:"img_num" gorm:"column:img_num;comment:通道;size:2;"`
 	FileName   string `json:"file_name" form:"file_name" gorm:"column:file_name;comment:店铺备注;size:200;"`
 	url        string `json:"url" form:"url" gorm:"column:url;comment:店地址;size:200;"`
