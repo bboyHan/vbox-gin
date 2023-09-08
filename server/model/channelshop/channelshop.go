9023accc68334886924b856bc6ccc6abfb9ed60f
lianxiaobao <36557317+lianxiaobao@users.noreply.github.com>
2023-09-08 22:42:58 +0800

    fix bug int/bigint convert to smallint issue

--- server/model/channelshop/channelshop.go
+++ server/model/channelshop/channelshop.go
@@ -8,12 +8,12 @@ import (
 // ChannelShop 结构体
 type ChannelShop struct {
 	global.GVA_MODEL
-	Uid          *int          `json:"uid" form:"uid" gorm:"column:uid;comment:用户id;size:11;"`
+	Uid          *int          `json:"uid" form:"uid" gorm:"column:uid;comment:用户id;"`
 	Cid          string        `json:"cid" form:"cid" gorm:"column:cid;comment:通道ID;size:50;"`
 	Channel      string        `json:"channel" form:"channel" gorm:"column:channel;comment:通道;size:50;"`
 	Shop_remark  string        `json:"shop_remark" form:"shop_remark" gorm:"column:shop_remark;comment:店铺备注;size:50;"`
 	Address      string        `json:"address" form:"address" gorm:"column:address;comment:店地址;size:500;"`
-	Money        *int          `json:"money" form:"money" gorm:"column:money;comment:金额;size:11;"`
+	Money        *int          `json:"money" form:"money" gorm:"column:money;comment:金额;"`
 	Status       *int          `json:"status" form:"status" gorm:"column:status;comment:开关;size:11;"`
 	DisMoney     string        `json:"disMoney" gorm:"-"`
 	OpenAndClose string        `json:"openAndClose" gorm:"-"`
