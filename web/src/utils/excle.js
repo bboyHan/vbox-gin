6b9fdae66c73928f30a6a675cefd8a58bccd6e5c
lianxiaobao <36557317+lianxiaobao@users.noreply.github.com>
2023-09-27 17:39:11 +0800

    add excle util

---
+++ web/src/utils/excle.js
@@ -0,0 +1,14 @@
+import FileSaver from "file-saver";
+import XLSX from "xlsx";
+
+export const useExcle = (name, tableName) => {
+    //name表示生成excel的文件名     tableName表示表格的id
+    var sel = XLSX.utils.table_to_book(document.querySelector(tableName))
+    var selIn = XLSX.write(sel, { bookType: 'xlsx', bookSST: true, type: 'array' })
+    try {
+      FileSaver.saveAs(new Blob([selIn], { type: 'application/octet-stream' }), name)
+    } catch (e) {
+      if (typeof console !== 'undefined') console.log(e, selIn) 
+    }
+    return selIn
+}
